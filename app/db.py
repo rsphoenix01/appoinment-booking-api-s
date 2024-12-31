f24c059220ddd1165e7bd6ba5133feee34a7ea07
rsphoenix01 <73596661+rsphoenix01@users.noreply.github.com>
2024-12-31 14:04:50 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -12,8 +12,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 if not MONGO_URI:
     raise ValueError("MONGO_URI environment variable is not set.")
 
-# Create a context manager to handle MongoDB connection
-# in app/db.py
+
 
 
 
